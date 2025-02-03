70c2ace52872ef9d57b9b0028ecc87796b0f0a0e
SatsonV <takusamuelvivo@gmail.com>
2025-02-03 16:45:35 +0100

    initial commit

---
+++ TutorMeApp/Component/navBar.jsx
@@ -0,0 +1,17 @@
+function navBar(){
+
+    return (
+        <header>
+            <h1>TutorMe</h1>
+            <nav>
+                <ul>
+                    <li><a href = '#'>Home</a></li>
+                    <li><a href = '#'> About</a></li>
+                    <li><a href = '#'>Contact</a></li>
+                </ul> 
+            </nav>
+        </header>
+    );
+}
+
+export default navBar
